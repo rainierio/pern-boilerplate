fa64f9c57a3f0e4ed4614e5b8394e03b2ea190e1
Rainier Letidjawa <Rainierletidjawa@gmail.com>
2022-02-11 17:55:08 +0700

    working on email verification

--- server/src/routes/v1/index.js
+++ server/src/routes/v1/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
+const docsRoute = require('./docs.route');
+const config = require('../../config/config');
 
 const router = express.Router();
 const defaultRoutes = [
@@ -12,10 +14,27 @@ const defaultRoutes = [
     path: '/users',
     route: userRoute,
   },
+
+
+];
+
+const devRoutes = [
+  // routes available only in development mode
+  {
+    path: '/docs',
+    route: docsRoute,
+  },
 ];
 
 defaultRoutes.forEach((route) => {
   router.use(route.path, route.route);
 });
+
+/* istanbul ignore next */
+if (config.env === 'development') {
+  devRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+  });
+}
 
 module.exports = router;
