f18cdd48af80dee3ba4a619a3f37273ed4e9693c
Rainier Letidjawa <Rainierletidjawa@gmail.com>
2022-02-24 09:01:08 +0700

    User controller

--- server/src/routes/v1/user.route.js
+++ server/src/routes/v1/user.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const auth = require('../../middlewares/auth');
 const validate = require('../../middlewares/validate');
 const userValidation = require('../../validations');
-const userController = require('../../controllers/user.controller');
+const userController = require('../../controllers/user.controller'); 
 
 const router = express.Router();
 
