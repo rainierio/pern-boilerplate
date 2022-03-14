8348072ee5ccbeb8b594dd30b2d348e47c104efd
rainierio <rainierletidjawa@gmai.com>
2022-03-14 20:28:05 +0700

    add dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </div>
   );
