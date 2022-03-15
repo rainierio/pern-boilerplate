49cd8c0aa44f40f9f5294a5ee21f53f24cc14e8a
rainierio <rainierletidjawa@gmai.com>
2022-03-15 14:56:52 +0700

    add home page

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/Home" element={<HomePage />} />
       </Routes>
     </div>
   );
