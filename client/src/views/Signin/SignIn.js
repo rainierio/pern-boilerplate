309739d734286f0f1093102e3e12136337f49b09
rainierio <rainierletidjawa@gmai.com>
2022-03-16 17:42:14 +0700

    add eslint config

--- client/src/views/Signin/SignIn.js
+++ client/src/views/Signin/SignIn.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 // Chakra imports
 import {
   Box,
@@ -12,81 +12,50 @@ import {
   Switch,
   Text,
   useColorModeValue,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 // Assets
-import signInImage from "assets/img/signInImage.png";
+import signInImage from 'assets/img/signInImage.png';
 
 function SignIn() {
   // Chakra color mode
-  const titleColor = useColorModeValue("teal.300", "teal.200");
-  const textColor = useColorModeValue("gray.400", "white");
+  const titleColor = useColorModeValue('teal.300', 'teal.200');
+  const textColor = useColorModeValue('gray.400', 'white');
   return (
     <Flex position="relative" mb="40px">
       <Flex
-        h={{ sm: "initial", md: "75vh", lg: "85vh" }}
+        h={{ sm: 'initial', md: '75vh', lg: '85vh' }}
         w="100%"
         maxW="1044px"
         mx="auto"
         justifyContent="space-between"
         mb="30px"
-        pt={{ sm: "100px", md: "0px" }}
+        pt={{ sm: '100px', md: '0px' }}
       >
         <Flex
           alignItems="center"
           justifyContent="start"
-          style={{ userSelect: "none" }}
-          w={{ base: "100%", md: "50%", lg: "42%" }}
+          style={{ userSelect: 'none' }}
+          w={{ base: '100%', md: '50%', lg: '42%' }}
         >
-          <Flex
-            direction="column"
-            w="100%"
-            background="transparent"
-            p="48px"
-            mt={{ md: "150px", lg: "80px" }}
-          >
+          <Flex direction="column" w="100%" background="transparent" p="48px" mt={{ md: '150px', lg: '80px' }}>
             <Heading color={titleColor} fontSize="32px" mb="10px">
               Welcome Back
             </Heading>
-            <Text
-              mb="36px"
-              ms="4px"
-              color={textColor}
-              fontWeight="bold"
-              fontSize="14px"
-            >
-              Enter your email and password to sign in
+            <Text mb="36px" ms="4px" color={textColor} fontWeight="bold" fontSize="14px">
+              Enter your email and password to sign in or simply use your connected account
             </Text>
             <FormControl>
               <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
                 Email
               </FormLabel>
-              <Input
-                borderRadius="15px"
-                mb="24px"
-                fontSize="sm"
-                type="text"
-                placeholder="Your email adress"
-                size="lg"
-              />
+              <Input borderRadius="15px" mb="24px" fontSize="sm" type="text" placeholder="Your email adress" size="lg" />
               <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
                 Password
               </FormLabel>
-              <Input
-                borderRadius="15px"
-                mb="36px"
-                fontSize="sm"
-                type="password"
-                placeholder="Your password"
-                size="lg"
-              />
+              <Input borderRadius="15px" mb="36px" fontSize="sm" type="password" placeholder="Your password" size="lg" />
               <FormControl display="flex" alignItems="center">
                 <Switch id="remember-login" colorScheme="teal" me="10px" />
-                <FormLabel
-                  htmlFor="remember-login"
-                  mb="0"
-                  ms="1"
-                  fontWeight="normal"
-                >
+                <FormLabel htmlFor="remember-login" mb="0" ms="1" fontWeight="normal">
                   Remember me
                 </FormLabel>
               </FormControl>
@@ -100,22 +69,16 @@ function SignIn() {
                 color="white"
                 mt="20px"
                 _hover={{
-                  bg: "teal.200",
+                  bg: 'teal.200',
                 }}
                 _active={{
-                  bg: "teal.400",
+                  bg: 'teal.400',
                 }}
               >
                 SIGN IN
               </Button>
             </FormControl>
-            <Flex
-              flexDirection="column"
-              justifyContent="center"
-              alignItems="center"
-              maxW="100%"
-              mt="0px"
-            >
+            <Flex flexDirection="column" justifyContent="center" alignItems="center" maxW="100%" mt="0px">
               <Text color={textColor} fontWeight="medium">
                 Don't have an account?
                 <Link color={titleColor} as="span" ms="5px" fontWeight="bold">
@@ -125,14 +88,7 @@ function SignIn() {
             </Flex>
           </Flex>
         </Flex>
-        <Box
-          display={{ base: "none", md: "block" }}
-          overflowX="hidden"
-          h="100%"
-          w="40vw"
-          position="absolute"
-          right="0px"
-        >
+        <Box display={{ base: 'none', md: 'block' }} overflowX="hidden" h="100%" w="40vw" position="absolute" right="0px">
           <Box
             bgImage={signInImage}
             w="100%"
@@ -141,7 +97,7 @@ function SignIn() {
             bgPosition="50%"
             position="absolute"
             borderBottomLeftRadius="20px"
-          ></Box>
+          />
         </Box>
       </Flex>
     </Flex>
