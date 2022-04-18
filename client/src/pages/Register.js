dc05943cce214c048fc9c7f83c477b4a99afb398
Rainier Letidjawa <Rainierletidjawa@gmail.com>
2022-04-18 21:51:03 +0700

    update pages

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,6 +2,7 @@ function name(params) {
     return(
         <div>
             {"put something to work on"}
+            {"I am still focus working on As400, don't have much time to finish this"}
         </div>
     )
 }
